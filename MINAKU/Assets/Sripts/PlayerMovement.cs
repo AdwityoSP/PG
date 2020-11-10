b936a88d42892263c69b3cfe488669a96e4bf624
Muhammad Ammar Nugraha <ammarcoolz3@gmail.com>
2020-11-10 18:48:18 +0700

    Animasi

--- MINAKU/Assets/Sripts/PlayerMovement.cs
+++ MINAKU/Assets/Sripts/PlayerMovement.cs
@@ -2,34 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 	public CharacterController2D kontroler;
 	private float moveX;
 	public float kecepatanjalan = 20;
 	private bool lompat = false;
 	private bool nunduk = false;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		moveX = Input.GetAxisRaw ("Horizontal") * kecepatanjalan;
+	public Animator animasi;
 
-		if (Input.GetButtonDown ("Jump")) {
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		moveX = Input.GetAxisRaw("Horizontal") * kecepatanjalan;
+
+		animasi.SetFloat("Kecepatan", Mathf.Abs(moveX));
+
+		if (Input.GetButtonDown("Jump"))
+		{
 			lompat = true;
+			animasi.SetBool("Melompat", lompat);
 		}
 
-		if (Input.GetButtonDown ("Crouch")) {
+		if (Input.GetButtonDown("Crouch"))
+		{
 			nunduk = true;
-		} else if (Input.GetButtonUp ("Crouch")) {
+		}
+		else if (Input.GetButtonUp("Crouch"))
+		{
 			nunduk = false;
 		}
 	}
 
-	void FixedUpdate(){
-		kontroler.Move (moveX * Time.fixedDeltaTime, nunduk, lompat);
+	public void Mendarat()
+	{
+		animasi.SetBool("Melompat", false);
+	}
+
+	void FixedUpdate()
+	{
+		kontroler.Move(moveX * Time.fixedDeltaTime, nunduk, lompat);
 		lompat = false;
 	}
 }
